1de01b1db6fb57695092aa7dc90acffc0a480bb2
Yuichi <yuichi.kageyama@mondo.com.au>
2025-12-10 19:54:19 +1100

    mapping for opening diag

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,3 +27,6 @@ map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Diagnostics
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open diagnostic floating window' })
