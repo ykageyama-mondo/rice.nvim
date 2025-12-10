a970dc0fc246595789b85843a948ef79a7742975
Yuichi <yuichi.kageyama@mondo.com.au>
2025-12-10 19:53:29 +1100

    better completions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -80,14 +80,22 @@ return {
         nerd_font_variant = 'mono',
       },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets' },
+        per_filetype = {
+          lua = { inherit_defaults = true, 'lazydev' },
+        },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
       },
-      snippets = { preset = 'luasnip' },
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
       signature = { enabled = true },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 1000,
+        },
+      },
     },
   },
 
